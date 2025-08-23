3f4756a6f981e5a14d992e9e23ed4d1015755ed1
Mattia Zanella <mattia_zanella@icloud.com>
2025-08-23 18:55:36 +0200

    Apply layout folders

--- app/app.vue
+++ app/app.vue
@@ -1,7 +1,7 @@
-<script setup lang="ts">
-const { data } = await useFetch('/api/menu')
-</script>
-
 <template>
-    <pre>{{ data }}</pre>
+  <main>
+    <NuxtLayout>
+      <NuxtPage />
+    </NuxtLayout>
+  </main>
 </template>
---
+++ app/components/Header.vue
@@ -0,0 +1,7 @@
+<script setup lang="ts">
+const { data } = await useFetch('/api/menu')
+</script>
+
+<template>
+    <pre>{{ data }}</pre>
+</template>
