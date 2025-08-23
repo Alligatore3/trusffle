373e3bc26610f39803293491bf28935f81cbee16
Mattia Zanella <mattia_zanella@icloud.com>
2025-08-23 19:46:50 +0200

    Add Base UNavigationMenu

--- app/components/Header.vue
+++ app/components/Header.vue
@@ -1,7 +1,14 @@
 <script setup lang="ts">
+import type { NavigationMenuItem } from '@nuxt/ui'
+
 const { data } = await useFetch('/api/menu')
+
+const menuItems = computed<NavigationMenuItem[]>(() => data.value?.data?.menu?.items.map(item => ({
+    url: item.title.toLocaleLowerCase().replaceAll(' ', '-'),
+    label: item.title,
+})) || [])
 </script>
 
 <template>
-    <pre>{{ data }}</pre>
+    <UNavigationMenu :items="menuItems" class="w-full justify-center" />
 </template>
