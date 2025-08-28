473d656441ee0172e257aa18b79c46d3b7acd842
Mattia Zanella <mattia_zanella@icloud.com>
2025-08-28 20:32:28 +0200

    Create products computed

--- app/components/HeroProducts.vue
+++ app/components/HeroProducts.vue
@@ -1,12 +1,27 @@
 <script setup lang="ts">
 const { data } = await useFetch("/api/products");
+
+const products = computed(
+  () =>
+    data.value?.data?.products?.nodes.map((node) => {
+      const images = node.images?.edges.map(({ node }) => ({
+        id: node?.id,
+        url: node?.url,
+      }));
+
+      return {
+        ...node,
+        images,
+      };
+    }) || []
+);
 </script>
 
 <template>
   <section>
     <div class="container mx-auto py-12">
       <pre>
-            {{ data }}
+            {{ products }}
         </pre
       >
     </div>
--- server/api/products.ts
+++ server/api/products.ts
@@ -1,21 +1,35 @@
 export default defineEventHandler(async () => {
   const storefront = useStorefront();
 
+  /**
+   * Retrieves a list of products in a store. Products are the items that merchants can sell in their store.
+   * @see https://shopify.dev/docs/api/admin-graphql/latest/queries/products
+   * @see https://shopify.dev/docs/api/admin-graphql/latest/objects/MediaEdge
+   */
   return await storefront.request(
     `#graphql
-        query FetchProducts($first: Int) {
-            products(first: $first) {
+        query FetchProducts($productsCount: Int, $imagesCount: Int) {
+            products(first: $productsCount) {
                 nodes {
                     id
                     title
                     description
+                    images (first: $imagesCount) {
+                        edges {
+                            node {
+                                id
+                                url
+                            }
+                        }
+                    }
                 }
             }
         }
     `,
     {
       variables: {
-        first: 3,
+        productsCount: 3,
+        imagesCount: 1,
       },
     }
   );
