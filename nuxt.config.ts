fd7f68ee2a14a5ed61a3c678fb026b862b073d3c
Mattia Zanella <mattia_zanella@icloud.com>
2025-10-30 21:40:33 +0100

    Reinstall nuxt tailwind

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,10 +1,15 @@
+import tailwindcss from "@tailwindcss/vite";
+
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: "2025-07-15",
   devtools: { enabled: true },
-  // css: ["~/assets/css/main.css"],
+  css: ["./app/assets/css/main.css"],
   ssr: false,
   modules: ["@nuxt/image", "@konkonam/nuxt-shopify"],
+  vite: {
+    plugins: [tailwindcss()],
+  },
   shopify: {
     name: "64d50c",
     clients: {
